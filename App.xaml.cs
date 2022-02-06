95b0ac0d2325325521cae40b41d7c967683a2fbc
Will Godwin <iamwill101400@gmail.com>
2022-02-05 21:46:57 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Toolbar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Application = System.Windows.Application;
+using System.Threading;
+
+namespace Toolbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MuteSymbol.Visibility = Visibility.Hidden;
+            UnmuteSymbol.Visibility = Visibility.Visible;      
+        }
+
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private async void MuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            MuteSymbol.Visibility = Visibility.Visible;
+            UnmuteSymbol.Visibility = Visibility.Hidden;
+            var audioController = new CoreAudioController();
+            var devices = await audioController.GetDevicesAsync(DeviceType.Capture, DeviceState.Active);
+            var device = devices.FirstOrDefault(x => x.IsDefaultDevice);
+            if (device != null)
+            {
+                device.ToggleMute();
+            }
+        }
+
+        private async void UnmuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            MuteSymbol.Visibility = Visibility.Hidden;
+            UnmuteSymbol.Visibility = Visibility.Visible;
+            var audioController = new CoreAudioController();
+            var devices = await audioController.GetDevicesAsync(DeviceType.Capture, DeviceState.Active);
+            var device = devices.FirstOrDefault(x => x.IsDefaultDevice);
+            if (device != null)
+            {
+                device.ToggleMute();
+            }
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WebsiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome.exe");
+        }
+
+        private void NoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+
+        private void FlightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            System.Diagnostics.Process.Start("https://fltplan.com/");
+        }
+    }
+}
+
+
